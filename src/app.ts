151d94f1ce124bfcadb2325de8aab58186a7878f
isaac545454 <isaac.newton1221@gmail.com>
2023-01-24 07:12:49 -0300

    feat: insert user test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("The sedulous hyena ate the antelope!");
@@ -8,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/users", (req: Request, res: Response) => {
   const users = [{ name: "john joe", email: "john@gmail.com" }];
   res.status(200).json(users);
+});
+
+app.post("/users", (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
