14c0469dca7fdd5f867cec2cc62532e89fbbddeb
isaac545454 <isaac.newton1221@gmail.com>
2023-01-24 06:58:53 -0300

    feat: expect users john

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,9 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("The sedulous hyena ate the antelope!");
 });
 
+app.get("/users", (req: Request, res: Response) => {
+  const users = [{ name: "john joe", email: "john@gmail.com" }];
+  res.status(200).json(users);
+});
+
 module.exports = app;
