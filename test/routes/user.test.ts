263622b10ce063d306255d9d376fd74b5d985a67
isaac545454 <isaac.newton1221@gmail.com>
2023-01-25 07:24:14 -0300

    test: test user database

--- test/routes/user.test.ts
+++ test/routes/user.test.ts
@@ -1,22 +1,27 @@
 import { response } from "express";
 import Request from "supertest";
 
-const app = require("../src/app");
+const app = require("../../src/app");
 
 test("deve listar os usuarios", () => {
   return Request(app)
     .get("/users")
     .then((response) => {
       expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(1);
-      expect(response.body[0]).toHaveProperty("name", "john joe");
+      expect(response.body.length).toBeGreaterThan(0);
+      expect(response.body[0]).toHaveProperty("name", "isaac");
     });
 });
 
 test("deve inserir o usuario com sucesso", () => {
+  const math = Math.random();
   return Request(app)
     .post("/users")
-    .send({ name: "isaac", email: "isaac@gmail.com" })
+    .send({
+      name: "isaac",
+      email: `isaac@gmail.com22222222222${math}`,
+      password: "123456",
+    })
     .then((response) => {
       expect(response.status).toBe(201);
       expect(response.body.name).toBe("isaac");
