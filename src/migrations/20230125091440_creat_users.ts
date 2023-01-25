fb192b087da48860fcefc921e4dfcac1aa59902f
isaac545454 <isaac.newton1221@gmail.com>
2023-01-25 06:36:19 -0300

    feat: create migrations user

--- src/migrations/20230125091440_creat_users.ts
+++ src/migrations/20230125091440_creat_users.ts
@@ -1,10 +1,14 @@
 import { Knex } from "knex";
 
+export const up = async (knex: Knex): Promise<void> => {
+  return knex.schema.createTable("users", (t) => {
+    t.increments("id").primary();
+    t.string("name").notNullable();
+    t.string("email").notNullable().unique();
+    t.string("password").notNullable();
+  });
+};
 
-export async function up(knex: Knex): Promise<void> {
-}
-
-
-export async function down(knex: Knex): Promise<void> {
-}
-
+export const down = async (knex: Knex): Promise<void> => {
+  return knex.schema.dropTable("users");
+};
