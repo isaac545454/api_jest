4516574c673f84de1c9bb44dabee7ddb835098f1
isaac545454 <isaac.newton1221@gmail.com>
2023-01-25 06:59:32 -0300

    refactor: router users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,17 @@
 import { Router, Request, Response } from "express";
 const router = Router();
+import { db } from "../app";
 
-router.get("/users", (req: Request, res: Response) => {
-  const users = [{ name: "john joe", email: "john@gmail.com" }];
+router.get("/users", async (req: Request, res: Response) => {
+  const users = await db("users").select();
   res.status(200).json(users);
 });
 
-router.post("/users", (req: Request, res: Response) => {
-  res.status(201).json(req.body);
+router.post("/users", async (req: Request, res: Response) => {
+  const createUser = await db("users").insert(req.body, "*");
+  console.log(createUser);
+
+  res.status(201).json(createUser[0]);
 });
 
 module.exports = router;
